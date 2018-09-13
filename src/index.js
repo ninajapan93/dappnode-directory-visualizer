5485360d6bf838336b30a83da9e82b6e0972eec2
Unknown <guillem.lion.benosa@gmail.com>
2018-09-13 18:01:50 +0200

    added web3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import getDirectory from "./API"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+getDirectory().then(directory => {
+    console.log(directory)
+})
